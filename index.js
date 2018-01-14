09e8985c83991d49e2534d1019bb80577053ab68
yuebaiXu <xyb1114@gmail.com>
2018-01-14 21:34:11 +0100

    add poet

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const EVENT_START_YEAR = 618;
+const ZOOM_IN_TIMES = 27;
+
 var EventModel = Backbone.Model.extend({
   defaults: {
     id: 0,
@@ -7,17 +10,34 @@ var EventModel = Backbone.Model.extend({
   }
 });
 
+var PoetModel = Backbone.Model.extend({
+  defaults: {
+    id: 0,
+    name: "",
+    start: 0,
+    end: 0
+  }
+});
+
 var EventCollection = Backbone.Collection.extend({
   url: "./data/events.json",
   model: EventModel
 });
 
+var PoetCollection = Backbone.Collection.extend({
+  url: "./data/poets.json",
+  model: PoetModel
+});
+
 var EventView = Backbone.View.extend({
+  initialize: function(options) {
+    //parameters need to be passed in variable options
+    this.nextYear = options.nextYear;
+  },
   events: {},
   tagName: "div",
   render: function() {
-    var height = 30;
-
+    var height = this.nextYear - this.model.get("year");
     var event = $.parseHTML("<div class='time-line'></div>");
     var timebar = $.parseHTML("<div class='time-bar'></div>");
     var timetext = $.parseHTML(
@@ -33,14 +53,14 @@ var EventView = Backbone.View.extend({
         "</div>"
     );
 
-    if (this.model.get("subcontent").length > 0) {
-      _.each(this.model.get("subcontent"), function(content) {
-        $(timetext).append("<div class='subcontent'>" + content + "</div>");
-      });
-    }
+    // if (this.model.get("subcontent").length > 0) {
+    //   _.each(this.model.get("subcontent"), function(content) {
+    //     $(timetext).append("<div class='subcontent'>" + content + "</div>");
+    //   });
+    // }
 
-    $(event).css("height", height * 5 + "px");
-
+    $(event).css("height", height * ZOOM_IN_TIMES + "px");
+    $(timebar).css("height", height * ZOOM_IN_TIMES + "px");
     $(event).append(timetext);
     $(event).append(timebar);
     this.$el.html(event);
@@ -55,23 +75,73 @@ var EventContainerView = Backbone.View.extend({
     this.collection.on("sync", this.render, this);
   },
   render: function() {
-    this.collection.each(function(model) {
+    var nextYear = this.collection.at(1).get("year");
+    this.collection.each(function(model, index) {
       var eventView = new EventView({
-        model: model
+        model: model,
+        nextYear: nextYear
       });
       this.$el.append(eventView.render().el);
+      nextYear = this.collection.at(index + 2).get("year");
+    }, this);
+  }
+});
+
+var PoetView = Backbone.View.extend({
+  className: "poet-text",
+  tagName: "div",
+  render: function() {
+    var distance = this.model.get("start") - EVENT_START_YEAR;
+    console.log(distance);
+    var poet = $.parseHTML(
+      "<div class='poet-name'>" +
+        this.model.get("name") +
+        "</div><div class='poet-year'>" +
+        this.model.get("start") +
+        "</div>"
+    );
+    this.$el.css("top", distance * ZOOM_IN_TIMES + "px");
+    this.$el.html(poet);
+    return this;
+  }
+});
+
+var PoetContainerView = Backbone.View.extend({
+  model: PoetModel,
+  initialize: function() {
+    this.collection.on("sync", this.render, this);
+  },
+  render: function() {
+    this.collection.each(function(model) {
+      var poetView = new PoetView({
+        model: model
+      });
+      $(".events-container").append(poetView.render().el);
     }, this);
   }
 });
 
 var eventCollection = new EventCollection();
+var poetCollection = new PoetCollection();
 var eventContainerView = new EventContainerView({
   collection: eventCollection
 });
-
+var poetContainerView = new PoetContainerView({
+  collection: poetCollection
+});
 eventCollection.fetch({
   success: function(collection, response) {
     console.log(collection);
     console.log(response);
+  }
+});
+
+poetCollection.fetch({
+  success: function(collection, response) {
+    console.log(collection);
+    console.log(response);
+  },
+  error: function() {
+    console.log("error in getting data");
   }
 });
